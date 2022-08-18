4b8180d9f58e67abc709d4937e3c8498337513ac
Andressa Siqueira <62267346+AndressaSiqueira@users.noreply.github.com>
2022-08-18 11:02:45 -0300

    Update funcao.js

--- funcao.js
+++ funcao.js
@@ -1,32 +1,79 @@
-const fdk = require('@fnproject/fdk');
-const common = require("oci-common")
-const streaming = require("oci-streaming")
-const os = require("oci-objectstorage");
-const fs = require("fs");
+const common = require("oci-common");
+const st = require("oci-streaming"); // OCI SDK package for OSS
 
-fdk.handle(async function (input) {
- try {
-const provider = new common.ConfigFileAuthenticationDetailsProvider();
-const client = new os.ObjectStorageClient({
-  authenticationDetailsProvider: provider
-});
+const ociConfigFile = "<config_file_path>";
+const ociProfileName = "<config_file_profile_name>";
+const ociMessageEndpointForStream = "<stream_message_endpoint>";
+const ociStreamOcid = "<stream_OCID>";
 
- const putObjectRequest = {
-      namespaceName: "id3kyspkytmr",
-      bucketName: "bucket",
-      putObjectBody: input,
-      objectName: object,
-      contentLength: stats.size
+// provide authentication for OCI and OSS
+const provider = new common.ConfigFileAuthenticationDetailsProvider(ociConfigFile, ociProfileName);
+  
+const consumerGroupName = "exampleGroup";
+const consumerGroupInstanceName = "exampleInstance-1";
+
+async function main() {
+  // OSS client to produce and consume messages from a Stream in OSS
+  const client = new st.StreamClient({ authenticationDetailsProvider: provider });
+  client.endpoint = ociMessageEndpointForStream;
+
+  // A cursor can be created as part of a consumer group.
+  // Committed offsets are managed for the group, and partitions
+  // are dynamically balanced amongst consumers in the group.
+  console.log("Starting a simple message loop with a group cursor");
+  const groupCursor = await getCursorByGroup(client, ociStreamOcid, consumerGroupName, consumerGroupInstanceName);
+  await consumerMsgLoop(client, ociStreamOcid, groupCursor);
+}
+
+main().catch((err) => {
+    console.log("Error occurred: ", err);
+}); 
+
+async function consumerMsgLoop(client, streamId, initialCursor) {
+    let cursor = initialCursor;
+    for (var i = 0; i < 10; i++) {
+      const getRequest = {
+        streamId: streamId,
+        cursor: cursor,
+        limit: 2
+      };
+      const response = await client.getMessages(getRequest);
+      console.log("Read %s messages.", response.items.length);
+      for (var message of response.items) {
+        if (message.key !== null)  {         
+            console.log("%s: %s",
+            Buffer.from(message.key, "base64").toString(),
+            Buffer.from(message.value, "base64").toString());
+        }
+       else{
+            console.log("Null: %s",
+                Buffer.from(message.value, "base64").toString() );
+       }
+      }
+      // getMessages is a throttled method; clients should retrieve sufficiently large message
+      // batches, as to avoid too many http requests.
+      await delay(2);
+      cursor = response.opcNextCursor;
+    }
+  }
+  
+
+async function getCursorByGroup(client, streamId, groupName, instanceName) {
+    console.log("Creating a cursor for group %s, instance %s.", groupName, instanceName);
+    const cursorDetails = {
+      groupName: groupName,
+      instanceName: instanceName,
+      type: st.models.CreateGroupCursorDetails.Type.TrimHorizon,
+      commitOnGet: true
     };
-    
-    const putObjectResponse = await client.putObject(putObjectRequest);
-    console.log("Put Object executed successfully" + putObjectResponse);
-
-    console.log("Fetch the object created");
-    const getObjectRequest = {
-      objectName: object,
-      bucketName: "bucket",
-      namespaceName: "id3kyspkytmr"
+    const createCursorRequest = {
+      createGroupCursorDetails: cursorDetails,
+      streamId: streamId
     };
-    const getObjectResponse = await client.getObject(getObjectRequest);
-    console.log("Get Object executed successfully.");
+    const response = await client.createGroupCursor(createCursorRequest);
+    return response.cursor.value;
+}
+
+async function delay(s) {
+    return new Promise(resolve => setTimeout(resolve, s * 1000));
+}
